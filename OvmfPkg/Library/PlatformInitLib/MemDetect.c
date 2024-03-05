b56f6743008f787a30fca82ec3adc422d4ea4af9
Ceping Sun <cepingx.sun@intel.com>
2024-03-05 08:30:36 +0800

    OvmfPkg/PlatformInitLib: Measure the memory size form QEMU
    Signed-off-by: Ceping Sun <cepingx.sun@intel.com>

--- OvmfPkg/Library/PlatformInitLib/MemDetect.c
+++ OvmfPkg/Library/PlatformInitLib/MemDetect.c
@@ -40,10 +40,17 @@ Module Name:
 #include <Library/QemuFwCfgLib.h>
 #include <Library/QemuFwCfgSimpleParserLib.h>
 #include <Library/TdxLib.h>
+#include <Library/TdxHelperLib.h>
 
 #include <Library/PlatformInitLib.h>
 
 #define MEGABYTE_SHIFT  20
+
+#define EV_POSTCODE_INFO_QEMU_RESERVED_MEMORY_END_DATA  "QEMU RESERVED MEMORY END"
+#define QEMU_RESERVED_MEMORY_END_DATA_LEN               (sizeof(EV_POSTCODE_INFO_QEMU_RESERVED_MEMORY_END_DATA) - 1)
+
+#define EV_POSTCODE_INFO_QEMU_X_PCIMMIO64MB_DATA  "QEMU X-PCIMMIO64MB"
+#define QEMU_X_PCIMMIO64MB_DATA_LEN               (sizeof(EV_POSTCODE_INFO_QEMU_X_PCIMMIO64MB_DATA) - 1)
 
 VOID
 EFIAPI
@@ -518,6 +525,21 @@ PlatformGetFirstNonAddress (
     case EFI_NOT_FOUND:
       break;
     case EFI_SUCCESS:
+
+
+      if (TdIsEnabled ()) {
+        //
+        // Measure the "opt/ovmf/X-PciMmio64Mb" which is downloaded from QEMU.
+        // It has to be done before it is consumed.
+        //
+        TdxHelperMeasureFwCfgData (
+          EV_POSTCODE_INFO_QEMU_X_PCIMMIO64MB_DATA,
+          QEMU_X_PCIMMIO64MB_DATA_LEN,
+          (VOID *)(UINTN)&FwCfgPciMmio64Mb,
+          sizeof (FwCfgPciMmio64Mb)
+          );
+      }
+
       if (FwCfgPciMmio64Mb <= 0x1000000) {
         PlatformInfoHob->PcdPciMmio64Size = LShiftU64 (FwCfgPciMmio64Mb, 20);
         break;
@@ -566,6 +588,20 @@ PlatformGetFirstNonAddress (
   if (!EFI_ERROR (Status) && (FwCfgSize == sizeof HotPlugMemoryEnd)) {
     QemuFwCfgSelectItem (FwCfgItem);
     QemuFwCfgReadBytes (FwCfgSize, &HotPlugMemoryEnd);
+
+    if (TdIsEnabled ()) {
+      //
+      // Measure the "etc/reserved-memory-end" which is downloaded from QEMU.
+      // It has to be done before it is consumed.
+      //
+      TdxHelperMeasureFwCfgData (
+        EV_POSTCODE_INFO_QEMU_RESERVED_MEMORY_END_DATA,
+        QEMU_RESERVED_MEMORY_END_DATA_LEN,
+        (VOID *)(UINTN)&HotPlugMemoryEnd,
+        FwCfgSize
+        );
+    }
+
     DEBUG ((
       DEBUG_VERBOSE,
       "%a: HotPlugMemoryEnd=0x%Lx\n",
